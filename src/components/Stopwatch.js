ca3d392575290e6fae46fd965a83abd3c6fc4b78
youba idjis <youba.etud@gmail.com>
2023-02-08 22:58:42 -0800

    adding timer

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,20 +1,61 @@
+import { useEffect, useState } from "react";
+
+ 
+
 const Stopwatch = () => {
+    const [isActive, setIsActive] = useState(false);
+    const [isPaused, setIsPaused] = useState(true);
+    const [time, setTime] = useState(0);
+
+    function Timer(props) {
+        return (
+          <div className="timer">
+            <span className="digits">
+              {("0" + Math.floor((props.time / 60000) % 60)).slice(-2)}:
+            </span>
+            <span className="digits">
+              {("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}.
+            </span>
+            <span className="digits mili-sec">
+              {("0" + ((props.time / 10) % 100)).slice(-2)}
+            </span>
+          </div>
+        );
+      }
+
+    useEffect(() => {
+        let interval = null;
+
+        if (isActive && isPaused === false) {
+            interval = setInterval(() => {
+                setTime((time) => time + 10);
+            }, 10);
+        } else {
+            clearInterval(interval);
+        }
+        return () => {
+            clearInterval(interval);
+        };
+    }, [isActive, isPaused]);
+
     const handleStart = () => {
-        console.log('watch Started')
+        setIsActive(true);
+        setIsPaused(false);
     }
-    const handleStop = () => {
-        console.log('watch Stoped')
+    const handlePause = () => {
+        setIsPaused(!isPaused);
     }
     const handleReset = () => {
-        console.log('watch reset')
+        setIsActive(false);
+        setTime(0);
     }
 
 
     return (
         <div className="stopwatch">
-            <h2>Stopwatch</h2>
+            <Timer time={time}/>
             <button id="start" onClick={() => { handleStart() }}>Start</button>
-            <button id="stop" onClick={() => { handleStop() }}>Stop</button>
+            <button id="stop" onClick={() => { handlePause() }}>Pause</button>
             <button id="reset" onClick={() => { handleReset() }}>Reset</button>
         </div>
     );
--- src/components/Workout.js
+++ src/components/Workout.js
@@ -1,21 +1,36 @@
 import { useState } from "react";
 
 const Workout = () => {
-    const [muscleOption, setMuscleOption]= useState('');
+
+    const [muscleOption, setMuscleOption] = useState('abdominals');
     const musclesList = ["abdominals", "abductors", "adductors", "biceps", "calves", "chest", "forearms", "glutes",
         "hamstrings", "lats", "lower_back", "middle_back", "neck", "quadriceps", "traps", "triceps"]
-    const handleSubmit = (e) => {
+    console.log(process.env)
+    function handleSubmit(e) {
         e.preventDefault();
-        console.log(muscleOption)
+           fetch("https://api.api-ninjas.com/v1/exercises?muscle="+muscleOption, {
+                method: 'GET',
+                mode: 'no-cors',
+                headers: {
+                    'X-Api-Key': process.env.API_KEY,
+                    "Content-Type": "Application/json",   
+                }
+            }).then(response => {
+                if (response.ok){
+                    console.log(response.json())
+                }
+                throw response
+            })
+
     }
     return (
         <div className="workout">
             <h1>workout</h1>
             <form className="form" onSubmit={(e) => { handleSubmit(e) }}>
                 <label>Choose targeted muscle: </label>
-                <select id="muscle" name="muscle" onChange={(e)=>{setMuscleOption(e.target.value)}}>
-                    {musclesList.map((muscle, index)=>
-                       <option key={index} value={muscle} >{muscle}</option>
+                <select id="muscle" name="muscle" onChange={(e) => { setMuscleOption(e.target.value) }}>
+                    {musclesList.map((muscle, index) =>
+                        <option key={index} value={muscle} >{muscle}</option>
                     )}
                 </select>
                 <button type="submit">Search</button>
