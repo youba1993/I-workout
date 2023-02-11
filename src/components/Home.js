d82384cee9bfad4fc574b20f0359538341156358
youba idjis <youba.etud@gmail.com>
2023-02-10 18:15:37 -0800

    Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
-
+import { useState } from "react";
+import Stopwatch from "./Stopwatch";
 
 function Home() {
+    const [nbrSets, setNbrSets] = useState(0);
+
     return (
         <div className="home">
             <div className='content'>
-                <h1>Home page</h1>
+                <h1>Welcome Buddy</h1>
+                <Stopwatch />
+                <h2> Number of Sets : </h2>
+                <div className="set" >
+                    <button id="minus" onClick={()=>setNbrSets(nbrSets-1)}>-</button>
+                    <h1>{nbrSets}</h1>
+                    <button id="plus" onClick={()=>setNbrSets(nbrSets+1)}>+</button>
+                </div>
             </div>
         </div>
     );
